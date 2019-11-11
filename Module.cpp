13168e218e719f03a0d9124aac3777794ca0e7ff
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2019-11-11 08:52:55 +0200

    initial

--- Module.cpp
+++ Module.cpp
@@ -3,9 +3,15 @@
 
 double factorial(int i);
 
+bool comparison(int firstNumber, int secondNumber);
+
+int getNumber(const int *arrUserNumbers, int digits, int num);
+
+bool findEquals(int digits, int *arrUserNumbers);
+
 using namespace std;
 
-void First(int eps, int k) {
+void Sum(int eps, int k) {
     double sum = 0;
     for (int i = k; i < 1000; i++) {
         double x = sum;
@@ -18,7 +24,7 @@ void First(int eps, int k) {
 }
 
 
-void Second(int n, int m) {
+void Triangle(int n, int m) {
     int x1, x2, x3, y1, y2, y3;
     for (int i = 0; i < n; i++) {
         x1 = rand() % (2 * m) - m;
@@ -38,7 +44,8 @@ void Second(int n, int m) {
 
         if (((length1 == length2) && (length2 == length3)) || ((length1 == length3) && (length1 == length2)) ||
             ((length2 == length3) && (length2 == length1))) {
-            cout << x1 << " " << y1 << endl << x2 << " " << y2 << endl << x3 << " " << y3 << endl;
+            cout << "A:" << x1 << " " << y1 << endl << "B:" << x2 << " " << y2 << endl << "C:" << x3 << " " << y3
+                 << endl;
         } else {
             cout << "not suitable" << endl;
         }
@@ -46,7 +53,7 @@ void Second(int n, int m) {
     }
 }
 
-void Third(int size1, int size2) {
+void newArray(int size1, int size2) {
     int k = 0;
     char arrayS[size1];
     char arrayT[size2];
@@ -72,10 +79,74 @@ void Third(int size1, int size2) {
     }
 }
 
+void Polinom(int number) {
+    int arrUserNumbers[number];
+    int numberOfElement = number / 2;
 
-void Five(int rows, int columns) {
+    for (int m = 0; m < 100; m++) {
+        for (int i = 0; i < number; i++) {
+            arrUserNumbers[i] = rand() % 4;
+        }
+        bool foundDuplicate = false;
+        for (int digits = 0; digits < numberOfElement; digits++) {
+            foundDuplicate = findEquals(digits, arrUserNumbers);
+            if (foundDuplicate) {
+                break;
+            }
+        }
+        if (!foundDuplicate) {
+            for (int i = 0; i < number; i++) {
+                cout << arrUserNumbers;
+            }
+            break;
+        }
+    }
+}
+
+bool findEquals(int digits, int *arrUserNumbers) {
+    bool flag = false;
+    int num = 0;
+    int firstNumber = getNumber(arrUserNumbers, digits, num);
+    do {
+        num++;
+        int secondNumber = getNumber(arrUserNumbers, digits, num);
+        if (secondNumber == -1) {
+            break;
+        }
+        bool comparisonResult = comparison(firstNumber, secondNumber);
+        if (comparisonResult) {
+            flag = true;
+            break;
+        } else {
+            firstNumber = secondNumber;
+        }
+    } while (true);
+    return flag;
+}
+
+bool comparison(int firstNumber, int secondNumber) {
+    return firstNumber == secondNumber;
+}
+
+int getNumber(const int *arrUserNumbers, int digits, int num) {
+    int size = sizeof(arrUserNumbers);
+    if (num * digits > size) {
+        return -1;
+    }
+    int sum = 0;
+    do {
+        for (int i = num * digits; i < num * digits * 2 - 1; i++) {
+            sum += arrUserNumbers[i] * pow(10, digits);
+            digits--;
+        }
+    } while (digits > 0);
+    return sum;
+}
+
+void sortOfMatrix(int rows, int columns) {
     int array[rows];
     int matrix[rows][columns + 1];
+    bool
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
             cout << " enter element of matrix:" << endl;
@@ -121,33 +192,39 @@ double factorial(int i) {
     }
 }
 
+
 int main() {
     double eps, k;
     cout << "enter eps:" << endl;
     cin >> eps;
     cout << "enter number k:" << endl;
     cin >> k;
-    First(eps, k);
+    Sum(eps, k);
 
     int n, m;
     cout << "enter number of triangles:" << endl;
     cin >> n;
     cout << "enter coordinate border:" << endl;
     cin >> m;
-    Second(n, m);
+    Triangle(n, m);
 
     int size1, size2;
     cout << "enter size of first array:" << endl;
     cin >> size1;
     cout << "enter size of second array:" << endl;
     cin >> size2;
-    Third(size1, size2);
+    newArray(size1, size2);
+
+    int number;
+    cout << "Enter number of elements:" << endl;
+    cin >> number;
+    Polinom(number);
 
     int rows, columns;
     cout << "enter number of rows:" << endl;
     cin >> rows;
     cout << "enter number of columns:" << endl;
     cin >> columns;
-    Five(rows, columns);
+    sortOfMatrix(rows, columns);
     return 0;
 }
